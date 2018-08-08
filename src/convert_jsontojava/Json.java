8fa081f624a88d3d1b70cd73307bf025f1857dfe
nahrens12 <nathanahrens12@outlook.com>
2018-08-08 16:26:27 +0100

    json

---
+++ src/convert_jsontojava/Json.java
@@ -0,0 +1,20 @@
+package convert_jsontojava;
+
+import java.util.Map;
+
+import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
+import com.google.gson.GsonBuilder;
+
+import account_admin.Account;
+
+public class Json {
+	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	
+	
+	public String JsonConverter(Map<String, Account> accountMap)
+	{
+		return gson.toJson(accountMap);
+	}
+
+}
